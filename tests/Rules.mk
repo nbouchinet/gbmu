7e794ce5f9712218d05191a8ead399731b67b768
Nicolas BOUCHINET <nbouchin@student.42.fr>
2019-02-08 12:55:27 +0100

    Change tests/Rules.mk to have linux google-test framework compilation compatibility

--- tests/Rules.mk
+++ tests/Rules.mk
@@ -2,15 +2,16 @@ TEST_CC			:= clang++
 TEST_CFLAGS		:=
 TEST_LFLAGS		:= -lgtest
 ifeq ($(shell uname -s), Darwin)
-TEST_CFLAGS		+= -I ~/.brew/include
-TEST_LFLAGS		+= -L ~/.brew/lib
+	TEST_CFLAGS		+= -I ~/.brew/include
+	TEST_LFLAGS		+= -L ~/.brew/lib
 endif
 COMPLINK		:= $(CC) $(TEST_CFLAGS) $(TEST_LFLAGS) -o
 
 CURR_DIR		:= tests
 ## Put all the tests here
-TEST_FILES		:= sample.cpp
-#
+TEST_FILES		:= 	sample.cpp \
+				Operations_utils.cpp \
+				Core.cpp
 
 TEST_OBJECTS	:= $(addprefix $(CURR_DIR)/$(OBJECT_DIR)/, $(TEST_FILES:.cpp=.o))
 RUN_TEST_RULES	:= $(addprefix test_, $(TEST_FILES:.cpp=))
@@ -31,18 +32,22 @@ clear_tests:
 	rm -f $(TEST_TARGETS)
 
 test_%: all $(TEST_OBJ_DIR) $(CURR_DIR)/%
+ifeq ($(shell uname -s), Darwin)
 	$(eval FAIL := $(shell script -q $*.out $(CURR_DIR)/$* > /dev/null; echo $$?))
+else
+	$(eval FAIL := $(shell script -qec $(CURR_DIR)/$* $*.out > /dev/null; echo $$?))
+endif
 	@if [ $(FAIL) -gt 0 ]; \
-	then \
+		then \
 		$(ECHO) "["$(RED)KO$(RESET)"] -" $*; \
 		cat $*.out; \
-	else \
+		else \
 		$(ECHO)  "["$(GREEN)OK$(RESET)"] -" $*; \
-	fi
+		fi
 	@rm $*.out
 
 $(CURR_DIR)/%: $(CURR_DIR)/$(OBJECT_DIR)/%.o $(NAME)
-	$(TEST_CC) $(TEST_LFLAGS) -o $@ $<
+	$(TEST_CC) -o $@ $< $(subst src/obj/main.o,,$(OBJECTS)) $(TEST_LFLAGS)
 
 $(CURR_DIR)/$(OBJECT_DIR)/%.o: $(CURR_DIR)/%.cpp
 	$(TEST_CC) $(CFLAGS) $(TEST_CFLAGS) -c -o $@ $<
