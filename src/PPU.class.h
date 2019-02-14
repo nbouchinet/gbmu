c9c7d8f7de368a8db760e62b807707eea5a3f01a
Florian DEXHEIMER <fdexheim@e1r7p22.42.fr>
2019-02-14 18:03:01 +0100

    rewriting of the render loop in progress with a pixel pipeline and a sprite tab for a line

--- src/PPU.class.h
+++ src/PPU.class.h
@@ -1,15 +1,29 @@
 #ifndef PPU_CLASS_H
 # define PPU_CLASS_H
 
-# define PPU_WIDTH 160
-# define PPU_HEIGHT 144
+# define LCD_WIDTH 160
+# define LCD_HEIGHT 144
+# define MAX_SPRITE_PER_LINE 10
 
 #include <iostream>
 #include <string>
 #include "ScreenOutput.class.h"
 #include "Fwd.hpp"
 
-uint8_t ReadMem(uint16_t addr);
+typedef struct		s_spriteInfo
+{
+	uint8_t			yPos;
+	uint8_t			xPos;
+	uint8_t			tileNumber;
+	uint8_t			flags;
+}					t_spriteInfo;
+
+typedef struct		s_pixelSegment
+{
+	uint8_t			value;
+	bool			isSprite;
+	t_spriteInfo	*spriteInfo;
+}					t_pixelSegment;
 
 class PPU
 {
@@ -21,37 +35,43 @@ public:
 	static void				switchPPUDebug(bool status);
 
 	bool					testBit(uint8_t byte, uint8_t bit_number);
-	void					renderScreen();
-	void					updateGraphics(int cycles);
-	void					updateStatus();
-	ScreenOutput			*getDriverScreen() { return (&driverScreen); } // UGO call dis to get screen
+	ScreenOutput			*getDriverScreen() { return (&_driverScreen); } // UGO call dis to get screen
 
 private:
 	uint16_t				getTileDataAddress(uint8_t tileIdentifier);
-	void					setupUsingWindow();
-	void					setupBackgroundMemory();
-	void					setupTileData();
+	void					setupWindow();
+	void					setupBackgroundMemoryStart();
+	void					setupSpriteAddressStart();
 	bool					isLCDEnabled();
 	void					renderScanLine();
-	void					setPixelDMG(uint8_t x, uint8_t y, uint8_t value);
+	void					setPixelDMG(uint8_t y, uint8_t x, uint8_t value);
+	void					finishLineBackgroundAsWindow();
 	void					renderTiles();
 	void					renderSprites();
+	void					getSpritesForLine();
 
-	ScreenOutput			driverScreen;
+	ScreenOutput			_driverScreen;
 	ComponentsContainer&	_components;
 
 
-	uint8_t					lcdc; // PPU controller register (0xFF41)
-	uint8_t					stat; // PPU status register (flags) (0xFF40)
-	uint8_t					scrollX;
-	uint8_t					scrollY;
-	uint8_t					currentScanline;
-	uint8_t					windowX;
-	uint8_t					windowY;
-	uint16_t				backgroundMemory;
-	uint16_t				tileDataStart;
-	bool					unsignedTileValues;
-	bool					usingWindow;
+	uint8_t					_lcdc;					// PPU controller register (0xFF41)
+	uint8_t					_stat;					// PPU status register (flags) (0xFF40)
+	uint8_t					_scrollX;				// Scroll of the screen in the memory (0-255)
+	uint8_t					_scrollY;				// Same as above (0-255)
+	uint8_t					_currentScanline;		// the line we're rendering, is a register
+	uint8_t					_windowX;				// Coordinates of the start of the window IN the screen (7-166)
+	uint8_t					_windowY;				// Same as above (0-143)
+	uint16_t				_backgroundDataStart;
+	uint16_t				_backgroundAttrStart;
+	uint16_t				_spriteDataStart;
+	uint16_t				_windowAttrStart;
+	uint8_t					_spriteSize;
+	bool					_unsignedTileValues;
+	bool					_windowingOn;
+
+	t_pixelSegment			_pixelPipeline[LCD_WIDTH];
+	t_spriteInfo			_spritesLine[MAX_SPRITE_PER_LINE]; // by default 10 sprites per line
+	uint8_t					_nbSprites;
 
 	static int				_nb_PPU;
 	static bool				_debug_PPU;
