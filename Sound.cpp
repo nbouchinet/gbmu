51adba47e6eeeb82d69643faf36c19fc90472cb0
Alaric SENAT <asenat@student.42.fr>
2019-02-15 17:57:30 +0100

    Sound modules units

--- Sound.cpp
+++ Sound.cpp
@@ -16,14 +16,83 @@ class IModulationUnit {
   virtual ~IModulationUnit() = default;
 
   virtual Byte trigger_steps() const = 0;
-  virtual void call() = 0;
+  virtual bool call() = 0;
 };
 
 class SweepUnit : public IModulationUnit {
-  public:
-    Byte trigger_steps() const override { return 0x44; /* 00100010 */ }
+ private:
+  Byte _sweep_period : 3;
+  Byte _negate : 1;
+  Byte _shift : 3;
+
+  Byte _current_period = 0;
+  Word& _frequency;
+
+ public:
+  SweepUnit() = delete;
+  SweepUnit(Word& frequency)
+      : _sweep_period(0), _negate(0), _shift(0), _frequency(frequency) {}
+
+  Byte trigger_steps() const override { return 0x44; /* 00100010 */ }
+
+  Byte get_register() const {
+    return (_sweep_period << 5) | (_negate << 3) | _shift;
+  }
+
+  bool call() override {
+    if (_current_period-- != 0 or _sweep_period == 0) return true;
+    _current_period = _sweep_period;
+    unsigned int new_freq = _frequency + (_frequency >> _shift) * -_negate;
+    if (new_freq > 0x7f) return false;
+    _frequency = new_freq;
+    return true;
+  }
 };
 
+class LengthUnit : public IModulationUnit {
+ private:
+  Word _length = 0;
+  bool _enabled = false;
+
+ public:
+  void set_length(Word l) { _length = l; }
+  void enable(bool enable = true) { _enabled = enable; }
+
+  Byte trigger_steps() const override { return 0x55; /* 01010101 */ }
+
+  bool call() override {
+    if (not _enabled) return true;
+    if (_length == 0) return false;
+    --_length;
+    return true;
+  }
+};
+
+class EnvelopeUnit : public IModulationUnit {
+ private:
+  Byte _negate : 1;
+  Byte _period : 3;
+  bool _enabled = false;
+  Byte _current_period = 0;
+  int& _volume;
+
+ public:
+  EnvelopeUnit() = delete;
+  EnvelopeUnit(int& volume) : _negate(0), _period(0), _volume(volume) {}
+
+  Byte trigger_steps() const override { return 0x80; /* 10000000 */ }
+
+  bool call() override {
+    if (not _enabled) return true;
+    if (_current_period-- != 0 or _period == 0) return true;
+    _current_period = _period;
+    if (_volume < 0 or _volume > 15)
+      _enabled = false;
+    else
+      _volume += (_negate) ? -1 : 1;
+    return true;
+  }
+};
 // -----------------------------------------------------------------------------
 
 class SoundChannel {
@@ -34,18 +103,20 @@ class SoundChannel {
   virtual void do_update() = 0;
 
  protected:
-  int p_output_volume;
-  bool p_muted;
-  SoundChannel(std::vector<ModUnitPtr>&& units)
-      : _modulation_units(std::move(units)), p_volume(0), p_muted(true) {}
+  int p_output_volume = 0;
+  bool p_muted = true;
+  bool p_enabled = true;
+
+  void bind_module(ModUnitPtr module) { _modulation_units.push_back(module); }
 
  public:
   virtual ~SoundChannel() = default;
 
   void update(unsigned int modulation_unit_step) {
+    if (not p_enabled) return;
     for (auto& modulation_unit : _modulation_units) {
       if (test_bit(modulation_unit_step, modulation_unit->trigger_steps()))
-        modulation_unit->call();
+        if (not modulation_unit->call()) p_enabled = false;
     }
     do_update();
   }
@@ -68,8 +139,9 @@ class SquareChannel : public SoundChannel {
   bool _init = false;
   int _volume = 0;
 
-  // SweepUnit _sweep;
+  SweepUnit _sweep;
   // LengthUnit _length;
+
   void do_update() override {
     if (_timer == 0) {
       assert(_frequency);
@@ -86,7 +158,7 @@ class SquareChannel : public SoundChannel {
   }
 
  public:
-  SquareChannel() : SoundChannel({}) {}
+  SquareChannel() : _sweep(_frequency) { bind_module(&_sweep); }
 };
 
 // -----------------------------------------------------------------------------
