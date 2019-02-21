c2db67f6306a98d3916e55b02d8e1304df24dd01
Florian DEXHEIMER <fdexheim@e1r6p11.42.fr>
2019-02-21 15:32:31 +0100

    adjusted MemoryBus to take new memory area in consideration, fixed color palettes writing (again...)

--- src/MemoryBus.cpp
+++ src/MemoryBus.cpp
@@ -17,6 +17,9 @@ MemoryBus::MemoryBus(ComponentsContainer &components) {
   push_component(0xFF04, 0xFF08, components.timer.get());
   push_component(0xFF0F, 0xFF0F, components.interrupt_controller.get());
   push_component(0xFF40, 0xFF4B, components.ppu.get());
+  push_component(0xFF51, 0xFF55, components.ppu.get());
+  push_component(0xFF68, 0xFF6B, components.ppu.get());
+  push_component(0x8000, 0x9FFF, components.ppu.get());
   push_component(0xFF80, 0xFFFE, components.core.get());
   push_component(0xFFFF, 0xFFFF, components.interrupt_controller.get());
   push_component(0xFF70, 0xFF70, components.unit_working_ram.get());
