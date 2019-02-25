c17a7bcfbabcc39e477c7e7df4223bb51f072f0a
Florian Dexheimer <fdexheim@student.42.fr>
2019-02-25 14:03:59 +0100

    fixed base init values and put init calls back in Gameboy.cpp

--- src/Gameboy.cpp
+++ src/Gameboy.cpp
@@ -75,6 +75,15 @@ void Gameboy::init() {
   _components.mem_bus->write(0xFF24, 0x77); // Sound4 Register
   _components.mem_bus->write(0xFF25, 0xF3); // Sound4 Register
   _components.mem_bus->write(0xFF26, 0xF1); // Sound4 Register
+  _components.mem_bus->write(0xFF40, 0x91); // LCD Register
+  _components.mem_bus->write(0xFF42, 0x00); // LCD Register
+  _components.mem_bus->write(0xFF43, 0x00); // LCD Register
+  _components.mem_bus->write(0xFF45, 0x00); // LCD Register
+  _components.mem_bus->write(0xFF47, 0xFC); // LCD Register
+  _components.mem_bus->write(0xFF48, 0xFF); // LCD Register
+  _components.mem_bus->write(0xFF49, 0xFF); // LCD Register
+  _components.mem_bus->write(0xFF4A, 0x00); // LCD Register
+  _components.mem_bus->write(0xFF4B, 0x00); // LCD Register
   _components.mem_bus->write(0xFFFF, 0x00);
 }
 
