9d77121089a7c48a2b2eb302ff8ad128c5de1783
Oguzhan YAGCI <oyagci@student.42.fr>
2019-02-25 16:22:10 +0100

    Reorder instructions

--- src/MemoryBus.cpp
+++ src/MemoryBus.cpp
@@ -13,6 +13,7 @@
 
 MemoryBus::MemoryBus(ComponentsContainer &components) {
   push_component(0x0000, 0x7FFF, components.cartridge.get());
+  push_component(0x8000, 0x9FFF, components.ppu.get());
   push_component(0xA000, 0xBFFF, components.cartridge.get());
   push_component(0xC000, 0xDFFF, components.unit_working_ram.get());
   push_component(0xFF04, 0xFF08, components.timer.get());
@@ -20,8 +21,7 @@ MemoryBus::MemoryBus(ComponentsContainer &components) {
   push_component(0xFF40, 0xFF4B, components.ppu.get());
   push_component(0xFF51, 0xFF55, components.ppu.get());
   push_component(0xFF68, 0xFF6B, components.ppu.get());
-  push_component(0x8000, 0x9FFF, components.ppu.get());
+  push_component(0xFF70, 0xFF70, components.unit_working_ram.get());
   push_component(0xFF80, 0xFFFE, components.core.get());
   push_component(0xFFFF, 0xFFFF, components.interrupt_controller.get());
-  push_component(0xFF70, 0xFF70, components.unit_working_ram.get());
 }
