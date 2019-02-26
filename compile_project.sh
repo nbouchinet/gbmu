a1aaa3395a7b762dfc40af4539c8ab375545bd43
Nicolas BOUCHINET <nbouchin@student.42.fr>
2019-02-26 10:47:41 +0100

    Refactor compile_project.sh in order to compile for everybody

--- compile_project.sh
+++ compile_project.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-/sgoinfre/goinfre/Perso/hublanc/Qt/5.10.0/clang_64/bin/qmake /Users/hublanc/Documents/gbInput/GbmuUI/GbmuUI.pro -spec macx-clang CONFIG+=debug CONFIG+=x86_64 CONFIG+=qml_debug && "/usr/bin/make" -f /Users/hublanc/Documents/gbInput/Makefile qmake_all && make -j4 && ./GbmuUI.app/Contents/MacOS/GbmuUI
+NAME=$(whoami)
+PROJECT_PATH=$(pwd)
+
+/sgoinfre/goinfre/Perso/$NAME/Qt/5.10.0/clang_64/bin/qmake $PROJECT_PATH/GbmuUI/GbmuUI.pro -spec macx-clang CONFIG+=debug CONFIG+=x86_64 CONFIG+=qml_debug && "/usr/bin/make" -f $PROJECT_PATH/Makefile qmake_all && make -j5 && ./GbmuUI.app/Contents/MacOS/GbmuUI
