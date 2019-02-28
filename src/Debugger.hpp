31236af50cdf5d98eaeebe17c696518bc8aef284
Nicolas BOUCHINET <nbouchin@student.42.fr>
2019-02-28 18:32:34 +0100

    add load

--- src/Debugger.hpp
+++ src/Debugger.hpp
@@ -344,7 +344,7 @@ public:
   bool get_run_duration() const { return _run_duration; }
   bool get_run_frame() const { return _run_one_frame; }
   bool get_run_step() const { return _run_one_step; }
-  bool get_lock() const { return _lock; }
+  bool get_lock() const { return _lock.load(); }
 
 private:
   void reset_flags();
