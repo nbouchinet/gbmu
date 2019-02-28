2dd219dc2f8fd9ae43e388d751f04e5f842c5bd9
Alaric SENAT <asenat@student.42.fr>
2019-02-28 10:35:00 +0100

    Putting the core::execute implementation into a whole new file to enhance compile time

--- src/cpu/Core.cpp
+++ src/cpu/Core.cpp
@@ -437,27 +437,6 @@ void Core::exec_instruction(std::function<void(Word &)> instr, Word addr,
   _cycles = clock_cycles;
 }
 
-void Core::execute(Core::Iterator it) {
-  if (_halt)
-    return;
-  it += _pc.word;
-  Iterator original_it = it;
-  auto fetch_word = [&]() -> Word {
-    Word ret = *it++;
-    ret |= *it++ << 8;
-    return ret;
-  };
-  switch (*it++) {
-#include "instructions.inc"
-  default:
-    break;
-  }
-  if (_in_jump_state)
-    _in_jump_state = false;
-  else
-    _pc.word += it - original_it;
-}
-
 // ----------------------------------------------------------------------------
 // Read and write
 // ----------------------------------------------------------------------------
