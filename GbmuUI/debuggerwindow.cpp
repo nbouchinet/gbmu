8f8394584d82a28e779ba4896b22f7b77b085b3d
Hugo BLANC <hublanc@student.42.fr>
2019-02-28 16:46:58 +0100

    watchpoint and memory map added in debugger

--- GbmuUI/debuggerwindow.cpp
+++ GbmuUI/debuggerwindow.cpp
@@ -72,9 +72,9 @@ DebuggerWindow::DebuggerWindow(QWidget *parent) :
 	}
 	
 	//Instructions pool size
-	ui->disassemblerWidget->setColumnCount(3);
+	ui->disassemblerWidget->setColumnCount(2);
 	titles.clear();
-	titles << "Address" << "Instruction" << "Data";
+	titles << "Instruction" << "Data";
 	ui->disassemblerWidget->setHorizontalHeaderLabels(titles);
 
 	ui->disassemblerWidget->setRowCount(6);
@@ -82,26 +82,37 @@ DebuggerWindow::DebuggerWindow(QWidget *parent) :
 	ui->disassemblerWidget->verticalHeader()->setSectionResizeMode(QHeaderView::Stretch);
 	refresh_instr();
 
+	//Memory Map
+	ui->memoryWidget->setColumnCount(16);
+	ui->memoryWidget->setRowCount(10);
+	titles.clear();
+	titles << "x0" << "x1" << "x2" << "x3" << "x4" << "x5" << "x6" << "x7" << "x8" << "x9" << "xA" << "xB" << "xC" << "xD" << "xE" << "xF";
+	ui->memoryWidget->setHorizontalHeaderLabels(titles);
+	ui->memoryWidget->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+	ui->memoryWidget->verticalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+	_addr_memory_map = 0x00;
+	refresh_memory_map();
+
 	//Breakpoints
 	ui->breakpointsWidget->setSelectionMode(QAbstractItemView::ExtendedSelection);
 	ui->breakpointsEdit->setMaxLength(4);
 
 	//Watchpoints
 	ui->watchpointsWidget->setSelectionMode(QAbstractItemView::ExtendedSelection);
+	ui->memoryLineEdit->setMaxLength(4);
 	connect(ui->registersWidget->verticalHeader(), &QHeaderView::sectionClicked, [this](int logicalIndex){
 		QString label = ui->registersWidget->verticalHeaderItem(logicalIndex)->text();
-		bool ok;
-    	QString text = QInputDialog::getText(this, tr("Add Watchpoint"), tr("Value:"), QLineEdit::Normal, "", &ok);
+		add_watchpoint(label, QString::number(logicalIndex));
 	});
 	connect(ui->videoRegistersWidget->verticalHeader(), &QHeaderView::sectionClicked, [this](int logicalIndex){
 		QString label = ui->videoRegistersWidget->verticalHeaderItem(logicalIndex)->text();
-		bool ok;
-    	QString text = QInputDialog::getText(this, tr("Add Watchpoint"), tr("Value:"), QLineEdit::Normal, "", &ok);
+		QString addrText = ui->videoRegistersWidget->item(logicalIndex, 0)->text();
+		add_watchpoint(label, addrText);
 	});
 	connect(ui->otherRegistersWidget->verticalHeader(), &QHeaderView::sectionClicked, [this](int logicalIndex){
 		QString label = ui->otherRegistersWidget->verticalHeaderItem(logicalIndex)->text();
-		bool ok;
-    	QString text = QInputDialog::getText(this, tr("Add Watchpoint"), tr("Value:"), QLineEdit::Normal, "", &ok);
+		QString addrText = ui->otherRegistersWidget->item(logicalIndex, 0)->text();
+		add_watchpoint(label, addrText);
 	});
 }
 
@@ -110,20 +121,42 @@ DebuggerWindow::~DebuggerWindow()
     delete ui;
 }
 
+void DebuggerWindow::refresh_memory_map()
+{
+	QStringList titles;
+	std::vector<Byte> memory_dump = g_gameboy.get_debugger().get_memory_dump(_addr_memory_map);
+	if (memory_dump.size() == 160)
+	{
+		int k = 0;
+		for (int i = 0; i < 10; i++)
+		{
+			titles << QString::number(_addr_memory_map + (16 * i), 16);
+			for (int j = 0; j < 17; j++)
+			{
+				ui->memoryWidget->setItem(i, j, new QTableWidgetItem(QString::number(memory_dump[k], 16)));
+				k++;
+			}
+		}
+		ui->memoryWidget->setVerticalHeaderLabels(titles);
+	}
+}
+
 void DebuggerWindow::refresh_instr()
 {
+	QStringList titles;
 	g_gameboy.get_debugger().set_instruction_pool_size(6);
 	std::vector<Debugger::_debug_info> instr_pool = g_gameboy.get_debugger().get_instruction_pool();
 	QString value;
 	int pool_size = instr_pool.size();
 	for (int i = 0; i < pool_size; i++){
-		ui->disassemblerWidget->setItem(i, 0, new QTableWidgetItem(QString::number(instr_pool[i].pc, 16)));
-		ui->disassemblerWidget->setItem(i, 1, new QTableWidgetItem(instr_pool[i].instr));
+		titles << QString::number(instr_pool[i].pc, 16);
+		ui->disassemblerWidget->setItem(i, 0, new QTableWidgetItem(instr_pool[i].instr));
 		value = QString::number(instr_pool[i].value[0], 16) + " " +
 			(instr_pool[i].size >= 2 ? QString::number(instr_pool[i].value[1], 16) : "") + " " +
 			(instr_pool[i].size == 3 ? QString::number(instr_pool[i].value[2], 16) : "");
-		ui->disassemblerWidget->setItem(i, 2, new QTableWidgetItem(value));
+		ui->disassemblerWidget->setItem(i, 1, new QTableWidgetItem(value));
 	}
+	ui->disassemblerWidget->setVerticalHeaderLabels(titles);
 }
 
 void DebuggerWindow::refresh_registers()
@@ -151,26 +184,46 @@ bool DebuggerWindow::duplicateInListWidgetItem(const QString &value, const QList
 	return (false);
 }
 
-void DebuggerWindow::addBreakpoint()
+void DebuggerWindow::add_watchpoint(const QString &label, const QString &addrText)
 {
-	if (ui->breakpointsEdit->text().length() == 4)
+	bool ok;
+    QString value = QInputDialog::getText(this, tr("Add Watchpoint"), tr("Value:"), QLineEdit::Normal, "", &ok);
+	if (!duplicateInListWidgetItem(label + ":" + addrText + ":" + value, ui->watchpointsWidget))
 	{
-		if (!duplicateInListWidgetItem(ui->breakpointsEdit->text(), ui->breakpointsWidget))
+		QRegExp hexMatcher("^[0-9A-F]{1,4}$", Qt::CaseInsensitive);
+		if (hexMatcher.exactMatch(value) || value.isEmpty())
 		{
-			QRegExp hexMatcher("^[0-9A-F]{1,4}$", Qt::CaseInsensitive);
-			if (hexMatcher.exactMatch(ui->breakpointsEdit->text()))
+			int32_t valueHex = value.isEmpty() ? -1 : value.toInt(&ok, 16);
+			if (ok)
 			{
-				bool ok;
-				uint16_t addr = ui->breakpointsEdit->text().toUInt(&ok, 16);
+				uint16_t addr = addrText.toUInt(&ok, 16);
 				if (ok)
 				{
-					g_gameboy.get_debugger().add_breakpoint(addr);
-					ui->breakpointsWidget->addItem(new QListWidgetItem(ui->breakpointsEdit->text()));
+					g_gameboy.get_debugger().add_watchpoint(addr, valueHex);
+					ui->watchpointsWidget->addItem(new QListWidgetItem(label + ":" + addrText + ":" + value));
 				}
 			}
 		}
-		ui->breakpointsEdit->clear();
 	}
+}
+
+void DebuggerWindow::addBreakpoint()
+{
+	if (!duplicateInListWidgetItem(ui->breakpointsEdit->text(), ui->breakpointsWidget))
+	{
+		QRegExp hexMatcher("^[0-9A-F]{1,4}$", Qt::CaseInsensitive);
+		if (hexMatcher.exactMatch(ui->breakpointsEdit->text()))
+		{
+			bool ok;
+			uint16_t addr = ui->breakpointsEdit->text().toUInt(&ok, 16);
+			if (ok)
+			{
+				g_gameboy.get_debugger().add_breakpoint(addr);
+				ui->breakpointsWidget->addItem(new QListWidgetItem(ui->breakpointsEdit->text()));
+			}
+		}
+	}
+	ui->breakpointsEdit->clear();
 }
 
 void DebuggerWindow::refresh_info()
@@ -230,30 +283,39 @@ void DebuggerWindow::on_DebuggerWindow_rejected()
 	g_gameboy.get_debugger().toggle();
 }
 
-void DebuggerWindow::on_registersWidget_cellClicked(int row, int column)
-{
-	(void)row;
-	(void)column;
-}
-
-void DebuggerWindow::on_registersWidget_itemClicked(QTableWidgetItem *item)
-{
-	qDebug("%s", qUtf8Printable(item->text()));
-}
-
 void DebuggerWindow::on_deleteWatchpointButton_clicked()
 {
-	/*
 	QList<QListWidgetItem*> items = ui->watchpointsWidget->selectedItems();
 	foreach(QListWidgetItem * item, items)
 	{
-		bool ok;
-		uint16_t addr = ui->breakpointsWidget->takeItem(ui->breakpointsWidget->row(item))->text().toUInt(&ok, 16);
-		if (ok)
+		bool okAddr;
+		bool okValue;
+		QStringList watchpointText = ui->watchpointsWidget->takeItem(ui->watchpointsWidget->row(item))->text().split(":");
+		uint16_t addr = watchpointText.at(1).toUInt(&okAddr, 16);
+		uint16_t value = watchpointText.at(2).toUInt(&okValue, 16);
+		if (okAddr && okValue)
 		{
-			g_gameboy.get_debugger().remove_breakpoint(addr);
-			delete ui->breakpointsWidget->takeItem(ui->breakpointsWidget->row(item));
+			g_gameboy.get_debugger().remove_watchpoint(addr, value);
+			delete ui->watchpointsWidget->takeItem(ui->watchpointsWidget->row(item));
 		}
 	}
-	*/
+}
+
+void DebuggerWindow::on_memoryLineEdit_editingFinished()
+{
+	QRegExp hexMatcher("^[0-9A-F]{1,4}$", Qt::CaseInsensitive);
+	if (hexMatcher.exactMatch(ui->memoryLineEdit->text()))
+	{
+		bool ok;
+		uint16_t addr = ui->memoryLineEdit->text().toUInt(&ok, 16);
+		if (ok)
+		{
+			addr &= 0xFFF0;
+			if (addr > 0xFF60)
+				addr = 0xFF60;
+			_addr_memory_map = addr;
+			refresh_memory_map();
+		}
+	}
+	ui->memoryLineEdit->clear();
 }
