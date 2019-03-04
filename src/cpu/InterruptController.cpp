24aa06ab018b3e00c6cd41b825663028ded4c574
nbouchin <40028878+nbouchin@users.noreply.github.com>
2019-03-04 12:53:51 +0100

    Change div register incrementation it should have a correct implement… (#73)
    * Change div register incrementation it should have a correct implementation
    * Norme interrupt and timer class

--- src/cpu/InterruptController.cpp
+++ src/cpu/InterruptController.cpp
@@ -5,34 +5,34 @@
 
 #include <iostream>
 
-InterruptController::InterruptController(ComponentsContainer& components)
+InterruptController::InterruptController(ComponentsContainer &components)
     : _components(components), _rIF(0), _rIE(0), _IME(false) {}
 
 void InterruptController::write(Word addr, Byte val) {
   switch (addr) {
-    case 0xFF0F:
-      _rIF = val;
-      break;
-    case 0xFFFF:
-      _rIE = val;
-      break;
-    default:
-      return;
+  case 0xFF0F:
+    _rIF = val;
+    break;
+  case 0xFFFF:
+    _rIE = val;
+    break;
+  default:
+    return;
   }
 }
 
 Byte InterruptController::read(Word addr) const {
   switch (addr) {
-    case 0xFF0F:
-      return (_rIF);
-    case 0xFFFF:
-      return (_rIE);
-    default:
-      return (0);
+  case 0xFF0F:
+    return (_rIF);
+  case 0xFFFF:
+    return (_rIE);
+  default:
+    return (0);
   }
 }
 
-void InterruptController::ParseInterrupt() {
+void InterruptController::parse_interrupt() {
   Byte rIF;
   Byte rIE;
 
@@ -43,7 +43,7 @@ void InterruptController::ParseInterrupt() {
       if ((rIF & 0x01) == (rIE & 0x01)) {
         _components.core->notify_interrupt();
         if (_IME)
-          ExecuteInterrupt(i);
+          execute_interrupt(i);
       }
       rIF >>= rIF;
       rIE >>= rIE;
@@ -51,43 +51,43 @@ void InterruptController::ParseInterrupt() {
   }
 }
 
-void InterruptController::ExecuteInterrupt(Byte interrupt) {
+void InterruptController::execute_interrupt(Byte interrupt) {
   _IME = false;
   _rIF = (_rIF & ~interrupt);
   switch (interrupt) {
-    case 0:
-      _components.core->instr_call(VBI);
-      break;
-    case 1:
-      _components.core->instr_call(LCDCSI);
-      break;
-    case 2:
-      _components.core->instr_call(TOI);
-      break;
-    case 3:
-      break;
-    case 4:
-      _components.core->instr_call(JOYI);
-      break;
+  case 0:
+    _components.core->instr_call(VBI);
+    break;
+  case 1:
+    _components.core->instr_call(LCDCSI);
+    break;
+  case 2:
+    _components.core->instr_call(TOI);
+    break;
+  case 3:
+    break;
+  case 4:
+    _components.core->instr_call(JOYI);
+    break;
   }
 }
 
-void InterruptController::RequestInterrupt(Word interrupt) {
+void InterruptController::request_interrupt(Word interrupt) {
   switch (interrupt) {
-    case VBI:
-      _rIF |= 1;
-      break;
-    case LCDCSI:
-      _rIF |= 2;
-      break;
-    case TOI:
-      _rIF |= 4;
-      break;
-    case STCI:
-      _rIF |= 8;
-      break;
-    case JOYI:
-      _rIF |= 16;
-      break;
+  case VBI:
+    _rIF |= 1;
+    break;
+  case LCDCSI:
+    _rIF |= 2;
+    break;
+  case TOI:
+    _rIF |= 4;
+    break;
+  case STCI:
+    _rIF |= 8;
+    break;
+  case JOYI:
+    _rIF |= 16;
+    break;
   }
 }
