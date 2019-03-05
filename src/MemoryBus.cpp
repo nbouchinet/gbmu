f9a69821fa9f414f19fad68601922e726e2ad3b5
Oguzhan YAGCI <oyagci@student.42.fr>
2019-03-05 14:35:53 +0100

    Load bios on startup

--- src/MemoryBus.cpp
+++ src/MemoryBus.cpp
@@ -1,18 +1,22 @@
 #include "MemoryBus.hpp"
 
-#include "src/Gameboy.hpp"
 #include "src/Cartridge.hpp"
-#include "src/cpu/Core.hpp"
-#include "src/cpu/InterruptController.hpp"
-#include "src/cpu/InputController.hpp"
-#include "src/cpu/Timer.hpp"
+#include "src/Gameboy.hpp"
 #include "src/LCDRegisters.hpp"
 #include "src/UnitWorkingRAM.hpp"
+#include "src/cpu/Core.hpp"
+#include "src/cpu/InputController.hpp"
+#include "src/cpu/InterruptController.hpp"
+#include "src/cpu/Timer.hpp"
+#include "src/Bios.hpp"
 
 #define push_component(begin, end, c)                                          \
   _ranged_components.push_back(RangedComponent{begin, end, c});
 
 MemoryBus::MemoryBus(ComponentsContainer &components) {
+  _cartridge = components.cartridge.get();
+
+  push_component(0x0000, 0x00FF, components.bios.get());
   push_component(0x0000, 0x7FFF, components.cartridge.get());
   push_component(0x8000, 0x9FFF, components.ppu.get());
   push_component(0xA000, 0xBFFF, components.cartridge.get());
@@ -27,4 +31,11 @@ MemoryBus::MemoryBus(ComponentsContainer &components) {
   push_component(0xFF80, 0xFFFE, components.core.get());
   push_component(0xFFFF, 0xFFFF, components.interrupt_controller.get());
   push_component(0xFF00, 0xFF00, components.input_controller.get());
+}
+
+void MemoryBus::disable_bios() {
+  if (_bios_is_enabled) {
+	_ranged_components.erase(_ranged_components.begin());
+    _bios_is_enabled = false;
+  }
 }
