d87a41c55e2e0802de5dee6462ffbaf5ed2fdb63
Oguzhan YAGCI <oyagci@student.42.fr>
2019-03-07 08:52:39 +0100

    Fix Qt rendering of the screen

--- src/ui/gbmuscreen.cpp
+++ src/ui/gbmuscreen.cpp
@@ -5,31 +5,40 @@
 #include <stdlib.h>
 #include <time.h>
 
-GbmuScreen::GbmuScreen(QWidget *parent) : QGraphicsView(parent) {
-  srand(time(NULL));
-  _ptrParent = parent;
+GbmuScreen::GbmuScreen(QWidget *parent)
+    : QGraphicsView(parent), _parent(parent) {
+  // srand(time(NULL));
   setAutoFillBackground(false);
   move(FIT_VIEW_W, FIT_VIEW_H);
   resize(parent->width(), parent->height());
+
+  _scene = std::make_unique<QGraphicsScene>(this);
+
+  setScene(_scene.get());
 }
 
 void GbmuScreen::updateGbScreen(void) {
-  resize(_ptrParent->width(), _ptrParent->height());
+
+  if (_parent->width() != width() || _parent->height() != height())
+    resize(_parent->width(), _parent->height());
+
   QImage image = QImage(GB_WIDTH, GB_HEIGTH, QImage::Format_RGBA8888);
-  if (g_gameboy.is_screen_filled()) {
-    for (int i = 0; i < GB_WIDTH; i++) {
-      for (int j = 0; j < GB_HEIGTH; j++) {
-        // to get cancer please try this image.setPixel(i, j, qRgba(rand() % 255
-        // , rand() % 255, rand() % 255, 255));
-        image.setPixel(i, j, g_gameboy.get_pixel(j, i));
-      }
+  for (int i = 0; i < GB_WIDTH; i++) {
+    for (int j = 0; j < GB_HEIGTH; j++) {
+
+      uint32_t rgba = g_gameboy.get_pixel(j, i);
+
+      uint8_t r = (rgba & 0xFF000000) >> (3 * 8);
+      uint8_t g = (rgba & 0x00FF0000) >> (2 * 8);
+      uint8_t b = (rgba & 0x0000FF00) >> (1 * 8);
+      uint8_t a = (rgba & 0x000000FF) >> (0 * 8);
+
+      image.setPixel(i, j, qRgba(r, g, b, a));
     }
   }
-  QGraphicsScene *old = scene();
-  QGraphicsScene *scene = new QGraphicsScene(this);
+  image.setPixel(5, 50, 0x000000FF);
   QPixmap p =
       QPixmap::fromImage(image.scaled(width(), height() - SKIP_TOOLBAR));
-  scene->addPixmap(p);
-  setScene(scene);
-  delete old;
+  _scene->clear();
+  _scene->addPixmap(p);
 }
