0943a8ebbdc4532a82ab9cd3a2620e846177881c
Alaric SENAT <asenat@student.42.fr>
2019-03-26 18:55:35 +0100

    Sweep sound module fix

--- src/sound/APU.cpp
+++ src/sound/APU.cpp
@@ -6,9 +6,9 @@
 #include "src/sound/WaveChannel.hpp"
 #include "utils/Operations_utils.hpp"
 
+#include <unistd.h>
 #include <cassert>
 #include <iostream>
-#include <unistd.h>
 
 namespace sound {
 
@@ -16,12 +16,13 @@ APU::MemoryRangedChannel::~MemoryRangedChannel() {}
 
 APU::APU(AudioInterface *interface, ComponentsContainer &c)
     : _audio_interface(interface),
-      _comps(c), _channels{{
-                     {0xff10, 0xff14, std::make_unique<SquareChannel>(true)},
-                     {0xff15, 0xff19, std::make_unique<SquareChannel>(false)},
-                     {0xff1a, 0xff1e, std::make_unique<WaveChannel>(_wave_ram)},
-                     {0xff1f, 0xff23, std::make_unique<NoiseChannel>()},
-                 }} {
+      _comps(c),
+      _channels{{
+          {0xff10, 0xff14, std::make_unique<SquareChannel>(true)},
+          {0xff15, 0xff19, std::make_unique<SquareChannel>(false)},
+          {0xff1a, 0xff1e, std::make_unique<WaveChannel>(_wave_ram)},
+          {0xff1f, 0xff23, std::make_unique<NoiseChannel>()},
+      }} {
   _last_dump = std::chrono::system_clock::now();
 }
 
@@ -33,8 +34,7 @@ void APU::update_clock() {
       ranged_channel.channel->update_modules(_modulation_units_steps);
       ranged_channel.channel->update();
     }
-    if (++_modulation_units_steps >= 8)
-      _modulation_units_steps = 0;
+    if (++_modulation_units_steps >= 8) _modulation_units_steps = 0;
   } else if (--_downsampling_countdown > 0) {
     for (auto &ranged_channel : _channels) {
       ranged_channel.channel->update();
@@ -82,8 +82,7 @@ float APU::fetch_and_mix_samples(Byte enabled_channels, float vol) const {
 }
 
 void APU::update_clock(Word cycles) {
-  if (not _APU_on)
-    return;
+  if (not _APU_on) return;
   while (cycles--) {
     update_clock();
   }
@@ -105,17 +104,17 @@ Byte APU::read(Word addr) const {
       return ranged_channel.channel->read(addr);
   }
   switch (addr) {
-  case 0xff24:
-    return _vin_and_volumes;
-  case 0xff25:
-    return _channel_to_terminal_output;
-  case 0xff26: {
-    Byte ret = _APU_on;
-    ret = (ret << 7) | 0x70;
-    for (auto i = 0u; i < _channels.size(); ++i)
-      ret |= _channels[i].channel->is_enabled() << i;
-    return ret;
-  }
+    case 0xff24:
+      return _vin_and_volumes;
+    case 0xff25:
+      return _channel_to_terminal_output;
+    case 0xff26: {
+      Byte ret = _APU_on;
+      ret = (ret << 7) | 0x70;
+      for (auto i = 0u; i < _channels.size(); ++i)
+        ret |= _channels[i].channel->is_enabled() << i;
+      return ret;
+    }
   }
   if (addr >= 0xff30 and addr <= 0xff3f)
     return _wave_ram[addr & 0xf];
@@ -126,23 +125,31 @@ Byte APU::read(Word addr) const {
 }
 
 void APU::write(Word addr, Byte v) {
+  static std::map<Word, const char *> map{
+      {0xFF10, "NR10_REG"}, {0xFF11, "NR11_REG"}, {0xFF12, "NR12_REG"},
+      {0xFF13, "NR13_REG"}, {0xFF14, "NR14_REG"}, {0xFF16, "NR21_REG"},
+      {0xFF17, "NR22_REG"}, {0xFF18, "NR23_REG"}, {0xFF19, "NR24_REG"},
+      {0xFF1A, "NR30_REG"}, {0xFF1B, "NR31_REG"}, {0xFF1C, "NR32_REG"},
+      {0xFF1D, "NR33_REG"}, {0xFF1E, "NR34_REG"}, {0xFF20, "NR41_REG"},
+      {0xFF21, "NR42_REG"}, {0xFF22, "NR43_REG"}, {0xFF23, "NR44_REG"},
+      {0xFF24, "NR50_REG"}, {0xFF25, "NR51_REG"}, {0xFF26, "NR52_REG"}};
   if (_dump) {
     auto now = std::chrono::system_clock::now();
     int ms =
         std::chrono::duration_cast<std::chrono::milliseconds>(now - _last_dump)
             .count();
-    std::cerr << std::hex << "APU write: " << addr << " -> " << +v
-              << " | delay: " << ms << "\n";
-    if (addr == 0xff23 and v & 0x80)
-      dump();
+    if (ms) std::cerr << "delay(0x" << std::hex << ms << ");\n";
+    if (map.find(addr) != map.end()) {
+      std::cerr << std::hex << map[addr] << " = 0x" << +v << ";\n";
+      if (addr == 0xff23 and v & 0x80) dump();
+    }
     _last_dump = std::chrono::system_clock::now();
   }
   if (addr == 0xff26) {
     _APU_on = (v & 0x80) >> 7;
     if (not _APU_on) {
       for (auto &chan : _channels) {
-        if (not _APU_on)
-          chan.channel->clear();
+        if (not _APU_on) chan.channel->clear();
       }
       clear();
     }
@@ -153,8 +160,7 @@ void APU::write(Word addr, Byte v) {
     return;
   } else if (addr >= 0xff27 and addr <= 0xff2f)
     return;
-  if (not _APU_on)
-    return;
+  if (not _APU_on) return;
   for (auto &ranged_channel : _channels) {
     if (addr >= ranged_channel.begin and addr <= ranged_channel.end) {
       ranged_channel.channel->write(addr, v);
@@ -162,14 +168,15 @@ void APU::write(Word addr, Byte v) {
     }
   }
   switch (addr) {
-  case 0xff24:
-    _vin_and_volumes = v;
-    return;
-  case 0xff25:
-    _channel_to_terminal_output = v;
-    return;
+    case 0xff24:
+      _vin_and_volumes = v;
+      return;
+    case 0xff25:
+      _channel_to_terminal_output = v;
+      return;
   }
   return;
   assert(false);
 }
-} // namespace sound
+}  // namespace sound
+
