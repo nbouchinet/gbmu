ef086d3bce713b9e1d8baed33766e7cfb5dea06b
Alaric SENAT <asenat@student.42.fr>
2019-03-29 17:33:08 +0100

    - Logger refactor - reset APU correctly - now works when no audio device is available

--- src/sound/AudioInterface.hpp
+++ src/sound/AudioInterface.hpp
@@ -14,9 +14,13 @@ class AudioInterface {
   using Sample = float;
   using MonoSamples = std::array<Sample, SamplesTableSize>;
 
+  struct StereoSample {
+    Sample left, right;
+  };
+
   virtual ~AudioInterface() = default;
   virtual bool queue_stereo_samples(const MonoSamples&, const MonoSamples&) = 0;
-  virtual float mix(const std::vector<float>&, float volume) const = 0;
+  virtual float mix(const std::vector<float>&) const = 0;
   virtual void start() = 0;
   virtual void terminate() = 0;
 };
