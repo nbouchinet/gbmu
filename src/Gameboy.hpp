b2fbcf330ab6629e743c9177dc2f4f3866dbabf9
Nicolas Bouchinet <nicolas.bouchinet@protonmail.com>
2019-04-01 18:20:03 +0200

    Double speed (#118)
    * Add components method that return a component
    * components getter is now const

--- src/Gameboy.hpp
+++ src/Gameboy.hpp
@@ -64,6 +64,7 @@ class Gameboy {
   void reset();
   void save(std::string save_name);
   void load_save(std::string save_name);
+  const auto& components() const { return _components; }
   uint32_t get_pixel(uint8_t y, uint8_t x) {
     return (_components.driver_screen->get_rgba(y, x));
   }
