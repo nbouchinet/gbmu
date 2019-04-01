5221237448317c593ebc84c7140cea27d4abf669
Oguzhan YAGCI <oyagci@student.42.fr>
2019-04-01 11:21:50 +0200

    Fix HDMA transfers

--- src/PPU.cpp
+++ src/PPU.cpp
@@ -124,45 +124,55 @@ bool PPU::is_hdma_active() {
 
 //------------------------------------------------------------------------------
 void PPU::hdma_h_blank_step() {
-	//	std::cerr << "hdma_h_blank_step called" << std::endl;
-	for (int i = 0; i < 16; i++) {
-		_components.mem_bus->write(
-				_h_blank_hdma_dst_addr,
-				_components.mem_bus->read<Byte>(_h_blank_hdma_src_addr));
-		_h_blank_hdma_src_addr++;
-		_h_blank_hdma_dst_addr++;
-	}
-	_hdma5--;
+  //	std::cerr << "hdma_h_blank_step called" << std::endl;
+  for (int i = 0; i < 16; i++) {
+    _components.mem_bus->write(
+        _h_blank_hdma_dst_addr + 0x8000,
+        _components.mem_bus->read<Byte>(_h_blank_hdma_src_addr));
+    _h_blank_hdma_src_addr++;
+    _h_blank_hdma_dst_addr++;
+  }
+
+  _hdma1 = _h_blank_hdma_src_addr >> 8;
+  _hdma2 = _h_blank_hdma_src_addr & 0xFF;
+  _hdma3 = _h_blank_hdma_dst_addr >> 8;
+  _hdma4 = _h_blank_hdma_dst_addr & 0xFF;
+
+  _hdma5--;
 }
 
 //------------------------------------------------------------------------------
 void PPU::initiate_hdma_transfer(uint8_t hdma5_arg) {
-	if (test_bit(hdma5_arg, 7) == true) // we setup the h blank dma (1 step at each hblank)
-	{
-//		std::cerr << "h blank dma transfer initiated" << std::endl;
-	    unset_bit(hdma5_arg, 7);
-		_hdma5 = hdma5_arg;
-		_h_blank_hdma_src_addr = ((_hdma1 << 8) + _hdma2) & 0xFFF0;
-		_h_blank_hdma_dst_addr = 0x8000 | (((_hdma3 << 8) | _hdma4) & 0x1FF0);
-	}
-	else // general purpose dma : everything is tranfered at once rignt away
-	{
-//		std::cerr << "general purpose dma transfer initiated" << std::endl;
-		uint16_t addr_source = 0;
-		uint16_t addr_dest = 0;
-		uint16_t lines_to_transfer = 0;
+  if (test_bit(hdma5_arg, 7) == true) {
+    unset_bit(hdma5_arg, 7);
+    _hdma5 = hdma5_arg;
+    _h_blank_hdma_src_addr = ((_hdma1 << 8) + _hdma2) & 0xFFF0;
+    _h_blank_hdma_dst_addr = ((_hdma3 << 8) | _hdma4) & 0x1FF0;
+  } else // general purpose dma : everything is tranfered at once rignt away
+  {
+    uint16_t addr_source = 0;
+    uint16_t addr_dest = 0;
+    uint16_t lines_to_transfer = 0;
 
-		addr_source = ((_hdma1 << 8) | _hdma2) & 0xFFF0;
-		addr_dest = 0x8000 | (((_hdma3 << 8) | _hdma4) & 0x1FF0);
-		lines_to_transfer = _hdma5 & 0x7F;
+    addr_source = ((_hdma1 << 8) | _hdma2) & 0xFFF0;
+    addr_dest = ((_hdma3 << 8) | _hdma4) & 0x1FF0;
+    lines_to_transfer = ((hdma5_arg & 0x7F) + 1) * 16;
 
-		for (int i = 0; i < (lines_to_transfer + 1) * 16; i++)
-		{
-			_components.mem_bus->write(
-					addr_dest + i, _components.mem_bus->read<Byte>(addr_source + i));
-		}
-		_hdma5 = 0xFF;
-	}
+    for (int i = 0; i < lines_to_transfer; i++) {
+      _components.mem_bus->write(
+          0x8000 + addr_dest + i,
+          _components.mem_bus->read<Byte>(addr_source + i));
+    }
+
+    addr_source += lines_to_transfer;
+    addr_dest += lines_to_transfer;
+
+    _hdma1 = addr_source >> 8;
+    _hdma2 = addr_source & 0xFF;
+    _hdma3 = addr_dest >> 8;
+    _hdma4 = addr_dest & 0xFF;
+    _hdma5 = 0xFF;
+  }
 }
 
 //------------------------------------------------------------------------------
